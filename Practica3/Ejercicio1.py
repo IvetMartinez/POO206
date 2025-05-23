30c068c645295993476469bb1b5a6a4b42e9dc41
IvetMartinez <ivimar637@gmail.com>
2025-05-22 19:58:54 -0600

    Ejercicio1
    Escribir un programa que pida al usuario un número entero y muestre por
    pantalla si es par o impar.

--- Practica3/Ejercicio1.py
+++ Practica3/Ejercicio1.py
@@ -2,12 +2,14 @@
 Programa que pida al usuario un número entero y 
 muestre por pantalla si es par o impar 
 """
-a=0
-a=int(input("Introduce un número entero:"))
+try:
+   a=0
+   a=int(input("Introduce un número entero:"))
 
-if a % 2 == 0 :
+   if a % 2 == 0 :
     print("%d es par.")
-else:
+   else:
     print("%d es impar.")
 
-
+except ValueError:
+    print("Error: Se ingresó un valor que no es un número entero.")
